0048f2127c801cd070afbbd22a4b4302100dc795
sethidhruv188 <140970083+sethidhruv188@users.noreply.github.com>
2023-10-18 19:47:15 -0400

    Update CodingChallenge1.c

--- CodingChallenge1.c
+++ CodingChallenge1.c
@@ -1,90 +1,46 @@
-#include <cs50.h>
+#include <cs50.h> // These are the libraries you'd have to import basically for math operations, user input adn to iterate over strings
 #include <ctype.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+int main(void) // Main method
 {
-    string text = get_string("Text: ");
-
-    int letters = 0;
+    string text = get_string("Text: "); // Ask the user to enter text
+// declare necessary variables
+    int letters = 0; 
     int words = 1;
     int sentences = 0;
-    for (int i = 0; i < strlen(text); i++)
+    
+    for (int i = 0; i < strlen(text); i++) // Create a for loop from 0 to length of string (entered by user)
     {
-        if (isalpha(text[i]))
+        if (isalpha(text[i])) // If the for loop recognizes a letter while iterating through the string
         {
-            letters++;
+            letters++; // Increments letter variable
         }
-        else if (text[i] == ' ')
+        else if (text[i] == ' ') // If the loop recognizes a space
         {
-            words++;
+            words++; // Increments the words variable
         }
-        else if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        else if (text[i] == '.' || text[i] == '?' || text[i] == '!') // If text recognizes a punctuation
         {
-            sentences++;
+            sentences++; // Increments the sentences variable
         }
     }
-    float L = (float) letters / (float) words * 100;
-    float S = (float) sentences / (float) words * 100;
-    int index = round(0.0588 * L - 0.296 * S - 15.8);
+    float L = (float) letters / (float) words * 100; // Calculates Letters per 100 words
+    float S = (float) sentences / (float) words * 100; // Calculates Sentences per 100 words
+    int index = round(0.0588 * L - 0.296 * S - 15.8); // Calculates the index
 
-    if (index < 1)
+    if (index < 1) // If index is less than 1
     {
-        printf("Before Grade 1\n");
+        printf("Before Grade 1\n"); // Displays that grade level is less than 1
     }
-    else if (index > 16)
+    else if (index > 16) // If the index is greater than 16
     {
-        printf("Grade 16+\n");
+        printf("Grade 16+\n"); // Displays that grade level is 16+  
     }
-    else
+    else // If the index is not less than 1 and not greater than 16
     {
-        printf("Grade %i\n", index);
-    }
-}
-#include <cs50.h>
-#include <ctype.h>
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-
-int main(void)
-{
-    string text = get_string("Text: ");
-
-    int letters = 0;
-    int words = 1;
-    int sentences = 0;
-    for (int i = 0; i < strlen(text); i++)
-    {
-        if (isalpha(text[i]))
-        {
-            letters++;
-        }
-        else if (text[i] == ' ')
-        {
-            words++;
-        }
-        else if (text[i] == '.' || text[i] == '?' || text[i] == '!')
-        {
-            sentences++;
-        }
-    }
-    float L = (float) letters / (float) words * 100;
-    float S = (float) sentences / (float) words * 100;
-    int index = round(0.0588 * L - 0.296 * S - 15.8);
-
-    if (index < 1)
-    {
-        printf("Before Grade 1\n");
-    }
-    else if (index > 16)
-    {
-        printf("Grade 16+\n");
-    }
-    else
-    {
-        printf("Grade %i\n", index);
+        printf("Grade %i\n", index); // Displays this m
     }
 }
